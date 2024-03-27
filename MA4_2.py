6a31f30bd9b618aa9d400ddad55c367d976c7907
ZeeshanM96 <zeeshanmajeedabdul@gmail.com>
2024-03-27 01:30:08 +0100

    Updated MA4_2 code

--- MA4_2.py
+++ MA4_2.py
@@ -20,35 +20,75 @@ def fib_numba(n):
         return fib_numba(n-1) + fib_numba(n-2)
 
 def main():
-    ns = range(30, 46)  # Adjust as needed
-    times_py, times_numba, times_cpp = [], [], []
+    # Timings for n = 30 to 45
+    ns_1 = range(30, 46)
+    times_py_1, times_numba_1, times_cpp_1 = [], [], []
     
-    for n in ns:
+    for n in ns_1:
         # Time Python version
         start = time.perf_counter()
         fib_py(n)
-        times_py.append(time.perf_counter() - start)
+        times_py_1.append(time.perf_counter() - start)
         
         # Time Numba version
         start = time.perf_counter()
         fib_numba(n)
-        times_numba.append(time.perf_counter() - start)
+        times_numba_1.append(time.perf_counter() - start)
         
         # Time C++ version
         start = time.perf_counter()
         person = Person(n)  # Assumes Person class is already linked to fib method
         person.fib()
-        times_cpp.append(time.perf_counter() - start)
+        times_cpp_1.append(time.perf_counter() - start)
+    
+    # Timings for n = 20 to 30
+    ns_2 = range(20, 31)
+    times_py_2, times_numba_2 = [], []
+    
+    for n in ns_2:
+        # Time Python version
+        start = time.perf_counter()
+        fib_py(n)
+        times_py_2.append(time.perf_counter() - start)
+        
+        # Time Numba version
+        start = time.perf_counter()
+        fib_numba(n)
+        times_numba_2.append(time.perf_counter() - start)
+    
+    # Compute Fibonacci number for n = 47 using C++ and Numba
+    fib_cpp_47 = Person(47).fib()
+    fib_numba_47 = fib_numba(47)
     
     # Plotting
-    plt.figure()
-    plt.plot(ns, times_py, label='Python')
-    plt.plot(ns, times_numba, label='Numba')
-    plt.plot(ns, times_cpp, label='C++')
+    plt.figure(figsize=(12, 6))
+    
+    # Plot timings for n = 30 to 45
+    plt.subplot(2, 1, 1)
+    plt.plot(ns_1, times_py_1, label='Python')
+    plt.plot(ns_1, times_numba_1, label='Numba')
+    plt.plot(ns_1, times_cpp_1, label='C++')
     plt.xlabel('Fibonacci Number n')
     plt.ylabel('Time (seconds)')
+    plt.title('Timings for n = 30 to 45')
     plt.legend()
+    
+    # Plot timings for n = 20 to 30
+    plt.subplot(2, 1, 2)
+    plt.plot(ns_2, times_py_2, label='Python')
+    plt.plot(ns_2, times_numba_2, label='Numba')
+    plt.xlabel('Fibonacci Number n')
+    plt.ylabel('Time (seconds)')
+    plt.title('Timings for n = 20 to 30')
+    plt.legend()
+    
+    # Save the plot
+    plt.tight_layout()
     plt.savefig('fibonacci_timings.png')  # Save the plot
+    
+    # Print Fibonacci number for n = 47
+    print("Fibonacci number for n = 47 (C++):", fib_cpp_47)
+    print("Fibonacci number for n = 47 (Numba):", fib_numba_47)
 
 if __name__ == '__main__':
     main()
